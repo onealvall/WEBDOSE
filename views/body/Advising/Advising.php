a7e1628599a149133567157f3dbe540277ff7e3a
Aldren <aldrensanchez@sdca.edu.ph>
2018-10-23 14:03:30 +0800

    start

---
+++ views/body/Advising/Advising.php
@@ -0,0 +1,278 @@
+<section class="content" style="background-color: #fff;">
+	<div class="container-fluid">
+		<div class="block-header">
+			<h1></h1>
+		</div><!-- Basic Example -->
+		<div class="row">
+			<div class="col-md-3">
+				<div class="input-group">
+					<span class="input-group-addon"><i class="material-icons">person</i></span>
+					<div class="form-line">
+						<input class="form-control date" placeholder="Student Number" type="text">
+					</div>
+				</div>
+				<div class="SBorder">
+					<div class="input-group">
+						<div class="form-line">
+							<input class="form-control date" disabled placeholder="Programs" type="text" value="">
+						</div>
+						<div class="form-line">
+							<input class="form-control date" disabled placeholder="Major" type="text" value="">
+						</div>
+					</div>
+				</div><br>
+				<div class="">
+					<label>School Year:</label> <span>2018-2019</span><br>
+					<label>Semester:</label> <span>Second</span>
+				</div><br>
+				<div class="SBorder">
+					<select class="form-control show-tick" data-live-search="true" name="">
+						<option disabled selected>
+							Select Curicullum
+						</option>
+						<option>
+						</option>
+					</select><br>
+					<br>
+					<select class="form-control show-tick" data-live-search="true" name="">
+						<option disabled selected>
+							Select Section
+						</option>
+						<option>
+						</option>
+					</select><br>
+					<br>
+					<div class="text-center">
+						<div class="form-group">
+							<input class="with-gap" id="male" name="block" type="radio"> <label for="male">Block</label> <input class="with-gap" id="female" name="open" type="radio"> <label class="m-l-20" for="female">Open</label>
+						</div>
+					</div>
+				</div><br>
+				<div class="text-center">
+					<button class="btn btn-danger">Add Schedule</button>
+				</div>
+			</div>
+			<div class="col-md-9">
+				<div class="table panel panel-danger" style="overflow-x:auto;">
+					<table class="table table-bordered">
+						<thead>
+							<tr class="danger">
+								<th></th>
+								<th class="text-center">M</th>
+								<th class="text-center">T</th>
+								<th class="text-center">W</th>
+								<th class="text-center">TH</th>
+								<th class="text-center">F</th>
+								<th class="text-center">S</th>
+								<th class="text-center">A</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>7:00 AM</td>
+								<td></td>
+								<td class="SchedBorder">
+									<p>20130515</p>
+									<p>Oneal A. Vallecera</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+							</tr>
+							<tr>
+								<td>7:30AM</td>
+								<td></td>
+								<td></td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+								<td class="SchedBorder">
+									<p>Course Code</p>
+									<p>Instructor</p>
+								</td>
+							</tr>
+							<tr>
+								<td>8:00AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>8:30AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>9:00AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>9:30AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>10:00AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>10:30AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>11:00AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>11:30AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>12:00AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>12:30AM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>1:00PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>1:30PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>2:00PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>2:30PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>3:00PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>3:30PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>4:00PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>4:30PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>5:00PM</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
